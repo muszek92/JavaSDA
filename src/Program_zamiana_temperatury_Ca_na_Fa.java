2808ea5c3c510e665a29b3fd6c33e82d8ba53890
Piotr <piotrmucha@algrafo.pl>
2017-07-12 20:16:57 +0200

    Program do zamiany temperatury z stopni C na Fa.

---
+++ src/Program_zamiana_temperatury_Ca_na_Fa.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Program_zamiana_temperatury_Ca_na_Fa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("------------ Program do zamiany stopni C na Fa ------------");
+        System.out.println(" ----------- Wybierz interesujące Cię działanie -----------");
+        System.out.println("1. Zamiana C na Fa \n2. Zamiana Fa na C");
+
+        int i = scanner.nextInt();
+
+
+        switch (i) {
+            case 1:
+                float wynikFa = 0;
+                System.out.println("Podaj temperature w Celsiuszach: ");
+                float c = scanner.nextInt();
+                wynikFa = c * 9 / 5 + 32;
+
+                System.out.println("Wynik:" + wynikFa);
+                break;
+            case 2:
+                float wynikC = 0;
+                System.out.println("Podaj temperature w Fareinheitach: ");
+                float fa = scanner.nextInt();
+                wynikC =  (fa - 32) / (float)1.8;
+                System.out.println("Wynik:" + wynikC);
+                break;
+        }
+
+
+
+}
+}
