76b6aaa00c4951e0832becd38b071f500dbd008c
Piotr <piotrmucha@algrafo.pl>
2017-07-13 21:06:27 +0200

    Program obliczający odległość - nowa funkcjonalnosc

--- src/zadanie_1_oop/Main.java
+++ src/zadanie_1_oop/Main.java
@@ -11,14 +11,22 @@ public class Main {
         Point point_1 = new Point();
         Point point_2 = new Point();
 
-        System.out.println("Podaj pierwszą współrzędną pierwszego punktu: ");
+        System.out.println("Podaj  współrzędną X pierwszego punktu: ");
         point_1.x = scanner.nextInt();
-        System.out.println("Podaj drugą współrzędną pierwszego punktu: ");
+        System.out.println("Podaj  współrzędną Y pierwszego punktu: ");
         point_1.y = scanner.nextInt();
 
-        point_2.x = 2;
-        point_2.y = 17;
+        System.out.println();
+        System.out.println("Podano poprawne dane\n");
 
+        System.out.println("Podaj  współrzędną X drugiego punktu: ");
+        point_2.x = scanner.nextInt();
+        System.out.println("Podaj  współrzędną Y drugiego punktu: ");
+        point_2.y = scanner.nextInt();
+        System.out.println("Podano poprawne dane\n");
+        System.out.println();
+
+        System.out.println("------ Wyniki ------");
 
         System.out.println("Odległosc od pierwszego punktu:" + point_1.distanceFromOrigin() + "\nOdległośc od drugiego punktu: " + point_2.distanceFromOrigin());
     }
