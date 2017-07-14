c8d8008764f1f1cec239d8952f016a7fbf9488b3
Piotr <piotrmucha@algrafo.pl>
2017-07-14 20:57:24 +0200

    Wstęp do programu firma

---
+++ src/company/Company.java
@@ -0,0 +1,13 @@
+package company;
+
+/**
+ * Created by Piotr on 14.07.2017.
+ */
+public class Company {
+    private Employee [] employees;
+
+    public Company{}{
+        employees = new Employee[10]
+    }
+
+}
---
+++ src/company/Employee.java
@@ -0,0 +1,48 @@
+package company;
+
+
+public class Employee {
+
+        private  String name;
+        private String surname;
+        private int age;
+        private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 ){
+            System.out.println("Wiek nie moze byc mniejszy od 0");
+        }
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 0){
+            System.out.println("Wypłatna nie może być ujemna");
+        }else
+        this.salary = salary;
+    }
+}
